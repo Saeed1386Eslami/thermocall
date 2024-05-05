43e910cee95d56b9c43a88183568330141a98fab
saeed <iscom1357@yahoo.com>
2024-05-05 16:58:20 +0330

    Edit text field

--- lib/home/view/setup.dart
+++ lib/home/view/setup.dart
@@ -23,7 +23,7 @@ class SetupPage extends StatelessWidget {
               Spacer(),
               Center(
                 child: Text(
-                  'Setap',
+                  'Setup',
                   style: TextStyle(color: Color(0xFF002039)),
                 ),
               ),
@@ -37,135 +37,138 @@ class SetupPage extends StatelessWidget {
             ],
           ),
         ),
-        body: Padding(
-          padding: EdgeInsets.only(left: 16, right: 16 , bottom: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'ID : Th12345678',
-                style: TextStyle(
-                  fontSize: textSize,
-                  color: textColor,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 8),
-              ),
-              Text(
-                'Updated 1 minutes ago',
-                style: TextStyle(
-                  fontSize: textSize,
-                  color: textColor,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 40),
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Name",
-                  labelStyle: TextStyle(color: Colors.grey, fontSize: textSize),
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 16,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 16, right: 16 , bottom: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'ID : Th12345678',
+                  style: TextStyle(
+                    fontSize: textSize,
+                    color: textColor,
                   ),
                 ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 32)),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Description",
-                  labelStyle: TextStyle(color: Colors.grey, fontSize: textSize),
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 100,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                ),
+                Text(
+                  'Updated 1 minutes ago',
+                  style: TextStyle(
+                    fontSize: textSize,
+                    color: textColor,
                   ),
                 ),
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 16)),
-              Text("Set Temperature Range" , style: TextStyle(
-                fontSize: 16
-              ),),
-Padding(padding: EdgeInsets.only(bottom: 32)),
-              Row(
-                children: [
-                  Padding(padding: EdgeInsets.only( left: 100)),
-                  Text(
-                    "-65",
-                    style: TextStyle(color: Color(0XFF002039), fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 40),
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Name",
+                    labelStyle: TextStyle(color: Colors.grey, fontSize: textSize),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 16,
+                    ),
                   ),
-                  Padding(padding: EdgeInsets.only(left: 25)),
-
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(360),
-                    child: Container(
-                      height: 70,
-                      width: 70,
-                      color: Color(0xFFF1F9FF),
-                      child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Center(
-                          child: Text(
-                            "-60",
-                            style: TextStyle(
-                                color: Color(0XFF002039), fontSize: 20),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 32)),
+                TextField(
+                  keyboardType:TextInputType.multiline,
+                    maxLines: null,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Description",
+                    labelStyle: TextStyle(color: Colors.grey, fontSize: textSize),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 100,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 16)),
+                Text("Set Temperature Range" , style: TextStyle(
+                  fontSize: 16
+                ),),
+          Padding(padding: EdgeInsets.only(bottom: 32)),
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.only( left: 100)),
+                    Text(
+                      "-65",
+                      style: TextStyle(color: Color(0XFF002039), fontSize: 20),
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 25)),
+          
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(360),
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        color: Color(0xFFF1F9FF),
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Center(
+                            child: Text(
+                              "-60",
+                              style: TextStyle(
+                                  color: Color(0XFF002039), fontSize: 20),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "-50",
-                      style: TextStyle(color: Color(0XFF002039), fontSize: 20),
-                    ),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Row(
-                  children:[
                     Padding(
-                padding: EdgeInsets.only(left: 32),
-                child: ElevatedButton(onPressed: null, child: Text(
-                  'Cancel' , style: TextStyle(
-                  color: Color(0xFF3C71A0),
-                ),
-                ),
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                        ), side: BorderSide(
-                  width: 1,
-                  color: Color(0xFF3C71A0),
-                ),
-                          backgroundColor: Colors.white,
-                          fixedSize: const Size(133, 38)),
+                      padding: EdgeInsets.only(left: 25),
+                      child: Text(
+                        "-50",
+                        style: TextStyle(color: Color(0XFF002039), fontSize: 20),
+                      ),
                     ),
-              ),
-              Padding(padding: EdgeInsets.only(left: 16)),
-
-
-                ElevatedButton(onPressed: null, child: Text(
-                  'Save' , style: TextStyle(
-                  color: Colors.white,
+                  ],
                 ),
-                ),
+                Spacer(),
+                Row(
+                    children:[
+                      Padding(
+                  padding: EdgeInsets.only(left: 32),
+                  child: ElevatedButton(onPressed: null, child: Text(
+                    'Cancel' , style: TextStyle(
+                    color: Color(0xFF3C71A0),
+                  ),
+                  ),
                   style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                      borderRadius: BorderRadius.circular(8),
+                          ), side: BorderSide(
+                    width: 1,
+                    color: Color(0xFF3C71A0),
+                  ),
+                            backgroundColor: Colors.white,
+                            fixedSize: const Size(133, 38)),
                       ),
-                      backgroundColor: Color(0xFF3C71A0),
-                      fixedSize: const Size(133, 38)),
-                ),]
-              ),
-            ],
+                ),
+                Padding(padding: EdgeInsets.only(left: 16)),
+          
+          
+                  ElevatedButton(onPressed: null, child: Text(
+                    'Save' , style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  ),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        backgroundColor: Color(0xFF3C71A0),
+                        fixedSize: const Size(133, 38)),
+                  ),]
+                ),
+              ],
+            ),
           ),
         ),
       ),
